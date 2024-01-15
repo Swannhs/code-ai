11c82d78d89f73ed6e75bd1a16592b53f666240d
GitHub Action <action@github.com>
2024-01-15 06:58:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/swannhs/codeai/MyBundle.kt
+++ src/main/kotlin/com/github/swannhs/codeai/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.swannhs.codeai
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/swannhs/codeai/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swannhs/codeai/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.swannhs.codeai.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swannhs.codeai.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
